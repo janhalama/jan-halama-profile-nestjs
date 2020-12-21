66e18559876142afd5cdd93e376745f1efa874ca
Jan Halama <halama@jablotron.cz>
2020-12-21 01:39:29 +0100

    chore: remove unused imports

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Res } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { AppService } from './app.service';
 
